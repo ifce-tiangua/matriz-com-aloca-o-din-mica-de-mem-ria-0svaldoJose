2f94d38aefb6048277467fb9127014d7caa653de
Osvaldo <107486317+0svaldoJose@users.noreply.github.com>
2024-11-21 13:58:35 -0300

    01

--- main.c
+++ main.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+	int lin, col;
+	scanf("%d", &lin);
+	scanf("%d", &col); 
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+	int **matriz = (int**) malloc(lin * sizeof(int*));
+	
+	if(lin == 0 || col == 0) { 
+		matriz[0] = NULL;
+	}
+	else {
+		for(int i = 0; i < lin; i++) {
+			matriz[i] = (int*) malloc(col * sizeof(int));
+		}
+	}
+	
+	if(matriz[0] != NULL) {
+		
+		for(int i = 0; i < lin; i++) {
+			for(int j = 0; j < col; j++) {
+				scanf("%d", &matriz[i][j]);
+			}
+		}
 
-    return 0;
+		for(int i = 0; i < lin; i++) {
+			for(int j = 0; j < col; j++) {
+				if(j == col-1) {
+					printf("%d", matriz[i][j]);
+				}else{
+					printf("%d ", matriz[i][j]);
+				}
+			}
+			printf("\n");
+		}
+	} else { 
+		printf("[matriz vazia]\n");
+	}
+
+	for(int i = 0; i < lin; i++) {
+		free(matriz[i]);
+	}
+	free(matriz);
+	return 0;
 }
